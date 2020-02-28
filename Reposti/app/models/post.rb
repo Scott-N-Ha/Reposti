db4e7ca341dbcd2b81b61467205416a0cd1fe87f
Scott Ha <123viet123@gmail.com>
2020-02-28 14:54:46 -0800

    ActiveStorage and Post Table Setup

---
+++ Reposti/app/models/post.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id           :bigint           not null, primary key
+#  post_type_id :integer
+#  title        :string
+#  body         :string
+#  author_id    :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Post < ApplicationRecord
+  validates :post_type_id, :author_id, presence: true
+
+  belongs_to :author,
+    foreign_key: :author_id,
+    class_name: :User
+
+  belongs_to :post_type
+
+  has_many_attached :photos
+end
---
+++ Reposti/db/migrate/20200228221737_create_post_types.rb
@@ -0,0 +1,9 @@
+class CreatePostTypes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :post_types do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
