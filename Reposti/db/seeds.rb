b20d28e7dcb3c32bf8a32927c387a9fcbcc3e25d
Scott Ha <123viet123@gmail.com>
2020-02-28 14:54:46 -0800

    ActiveStorage and Post Table Setup

--- Reposti/db/seeds.rb
+++ Reposti/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+u1 = User.create(username: 'test', email: 'test@email.com', password: 'password')
+
+PostType.destroy_all
+
+text = PostType.create(name: 'text')
+photo = PostType.create(name: 'photo')
+quote = PostType.create(name: 'quote')
+link = PostType.create(name: 'link')
+chat = PostType.create(name: 'chat')
+audio = PostType.create(name: 'audio')
+video = PostType.create(name: 'video')
+
+Post.destroy_all
+
+p1 = Post.create(post_type_id: text.id, title: 'Test Text Post', body: 'The body of a the test text post', author_id: u1.id)
