05b8efdeaed25d116c6d9fd03014ce9d9a330ac6
Scott Ha <123viet123@gmail.com>
2020-02-28 11:06:45 -0800

    Controllers and Backend Auth Setup

--- Reposti/app/models/user.rb
+++ Reposti/app/models/user.rb
@@ -1,2 +1,44 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string
+#  email           :string
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_params)
+    user = User.find_by(username: user_params[:username])
+    user&.is_password?(user_params[:password]) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.update!(session_token: SecureRandom::urlsafe_base64)
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
 end
