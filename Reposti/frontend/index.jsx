f275d235389fc0958abae5b92a55dba62e6ae990
Scott Ha <123viet123@gmail.com>
2020-02-28 11:00:05 -0800

    Static Pages and Controllers generated

---
+++ Reposti/frontend/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <div>React is working</div>
+    , root);
+});
