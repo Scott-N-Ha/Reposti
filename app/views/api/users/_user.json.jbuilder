e488a69cc53b1de10d72091377a0975cc51aa5c8
Scott Ha <123viet123@gmail.com>
2020-03-09 09:58:44 -0700

    Abandoning settings page for now

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :username
+json.extract! user, :id, :username, :email
 json.profile_image_url user.profile_image.attached? ? url_for(user.profile_image) : ''
--- frontend/components/users/settings.jsx
+++ frontend/components/users/settings.jsx
@@ -6,10 +6,19 @@ export default class Settings extends React.Component {
     this.state = Object.assign({
       profile_image_url: "",
       profile_image: null,
+
+      editEmail: false,
+      editPassword: false,
+
     }, this.props.user);
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleImage = this.handleImage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.cancelEdit = this.cancelEdit.bind(this);
+
+    this.handleEmail = this.handleEmail.bind(this);
+    this.renderEmail = this.renderEmail.bind(this);
   }
 
   handleChange(e){
@@ -44,12 +53,13 @@ export default class Settings extends React.Component {
   handleSubmit(e){
     e.preventDefault();
 
-    const { username, password, profile_image } = this.state.user;
+    const { username, password, profile_image, email } = this.state.user;
     const { updateSingleUser, updateSingleUserWithImage } = this.props;
 
     if (profile_image !== null){
       let formData = new FormData();
       formData.append('user[username]', username);
+      formData.append('user[email]', email);
       formData.append('user[password]', password);
       formData.append('user[profile_image]', profile_image);
 
@@ -58,6 +68,7 @@ export default class Settings extends React.Component {
     } else {
       let newState = {
         username: username,
+        email: email,
         password: password,
       }
 
@@ -66,19 +77,43 @@ export default class Settings extends React.Component {
     }
   }
 
-  render(){
-    const { username, password, profile_image, profile_image_url } = this.props.user;
+  renderEmail(){
+    if (this.state.editEmail){
+      <form onSubmit={this.handleSubmit}>
+        <input type="email" name="email" onChange={this.handleChange} value={this.state.email} />
+        <button>Cancel</button>
+        <button>Save</button>
+      </form>
+    } else {
+      return (
+        <div className="settings-email">
+          {this.state.email} <i className="fas fa-edit" onClick={this.handleEmail} ></i>
+        </div>
+      )
+    }
+  }
 
+  handleEmail(e){
     debugger
+    this.setState = { editEmail: true };
+  }
 
-    if (user === undefined) return null;
+  render(){
+    const { username, profile_image_url } = this.props.user;
+
+    if (this.props.user === undefined) return null;
 
     return (
       <div className="settings-div">
-        <img
-          className="profile-pic-user-show"
-          src={ profile_image_url === "" ? "https://68.media.tumblr.com/9f9b498bf798ef43dddeaa78cec7b027/tumblr_o51oavbMDx1ugpbmuo7_500.png" : profile_image_url }
-          alt={username} />
+        <div className="settings-profile-img">
+          <img
+            className="profile-pic-user-show"
+            src={ profile_image_url === "" ? "https://68.media.tumblr.com/9f9b498bf798ef43dddeaa78cec7b027/tumblr_o51oavbMDx1ugpbmuo7_500.png" : profile_image_url }
+            alt={username} />
+          <input type="file" name="profile_image" id="photos-uploader" onChange={this.handleImage} accept="image/*" />
+        </div>
+
+        {this.renderEmail()}
       </div>
     )
   }
