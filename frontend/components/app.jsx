23b7802f3ba391ace83158e82b14fcc7e0c7856e
Scott Ha <123viet123@gmail.com>
2020-03-12 18:26:56 -0700

    Added a scroll to top button

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,10 +12,25 @@ import FollowingContainer from './follow/following_container.js';
 import LikesContainer from './like/likes_container.js';
 import PostShowContainer from './posts/post_show_container.js';
 
+function topFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function scroll(){
+  const btn = document.querySelector('.top-btn');
+
+  btn.style.display = document.body.scrollTop > 250 || document.documentElement.scrollTop > 250 ? "block" : "none";
+}
+
+window.onscroll = () => scroll();
+
 const App = (props) => {
   return (
   <div className="app">
     <NavBarContainer />
+
+    <i onClick={topFunction} className="fas fa-arrow-alt-circle-up top-btn rainbow-effect" style={{display: "none"}}></i>
 
     <Switch>
       <AuthRoute path="/login" component={LoginFormContainer} />
